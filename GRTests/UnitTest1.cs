8cbb980379904cb618c00b2aab4703e9f0d25d9b
Joaquin Olivar <joaquinolivar@gmail.com>
2021-03-17 11:33:37 -0400

    Fixed a button LMAO

---
+++ GRTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using GRDL;
+using GRModels;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace GRTests
+{
+    public class GRTests
+    {
+        private readonly DbContextOptions<GRDBContext> options;
+        [Fact]
+        public void
+        {
+
+        }
+
+}
